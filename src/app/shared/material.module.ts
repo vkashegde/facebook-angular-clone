b5b967641fe033c65e97c19bc2e76ac58ed7e61c
{IamVks} <{vkshegde11@gmail.com}>
2021-04-02 11:27:18 +0530

    login page design completed

--- src/app/shared/material.module.ts
+++ src/app/shared/material.module.ts
@@ -1,8 +1,18 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
+import { MatIcon, MatIconModule } from '@angular/material/icon';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatDialogModule } from '@angular/material/dialog';
 
-const modules: any[] = [MatCardModule];
+const modules: any[] = [
+  MatCardModule,
+  MatIconModule,
+  MatSnackBarModule,
+  MatProgressSpinnerModule,
+  MatDialogModule,
+];
 
 @NgModule({
   declarations: [],
